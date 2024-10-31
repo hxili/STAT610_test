bdb5cb1809f9a7f1b4dd3f5c112ca9a2511ab941
hxili <hxili@ucdavis.edu>
2024-10-30 22:47:19 -0400

    vectorization

--- llr_functions.R
+++ llr_functions.R
@@ -4,28 +4,32 @@ llr = function(x, y, z, omega) {
   return(fits)
 }
 
-# Compute f hat function:
+# Compute f_hat function with optimized calculations:
 compute_f_hat = function(z, x, y, omega) {
-  Wz = make_weight_matrix(z, x, omega)
+  Wz = make_weight_vector(z, x, omega)  # Now returns a vector
   X = make_predictor_matrix(x)
-  f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  
+  # Replace Wz %*% X and Wz %*% y with optimized calculations
+  WX = apply(X, 2, function(col) Wz * col)
+  Wy = Wz * y
+  
+  # Compute the components
+  XtWX = t(X) %*% WX
+  XtWy = t(X) %*% Wy
+  
+  # Calculate f_hat
+  f_hat = c(1, z) %*% solve(XtWX) %*% XtWy
   return(f_hat)
 }
 
-# Below is our task to write weight matrix function:
-make_weight_matrix = function(z, x, omega) {
-  # Define the weight function W(r)
+# Modify make_weight_matrix to return a vector instead:
+make_weight_vector = function(z, x, omega) {
   W = function(r) {
     ifelse(abs(r) < 1, (1 - abs(r)^3)^3, 0)
   }
-  
-  # Compute the weights for each point based on the distance to z
   distances = abs(x - z) / omega
   weights = W(distances)
-  
-  # Return a diagonal matrix with the weights
-  Wz = diag(weights)
-  return(Wz)
+  return(weights)
 }
 
 # Below is our task to write predictor matrix function:
